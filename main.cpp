f41240c941f9560bc8eb4b9defb03fb3adc96907
Laszlo Kocsis <kocsislaci95@gmail.com>
2023-12-06 15:06:53 +0100

    day 6 part 1

--- main.cpp
+++ main.cpp
@@ -1,161 +1,78 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <regex>
 
 using namespace std;
 
-int main() {
+const regex pattern("\\d+");
+
+int main()
+{
     ifstream inputFile;
-    inputFile.open("./input.txt");
+    inputFile.open("../input6.txt");
 
-    int result = 0;
+    smatch match;
     string line;
-    while (getline(inputFile, line)) {
-        int linePos = 0;
-        vector<int> foundNumbers = vector<int>();
-        while (linePos != line.size())
-        {
-            if (isdigit(line.at(linePos)))
-                foundNumbers.push_back(line.at(linePos) - 48); // offset to 0-9
-            else
-            {
 
-                if (line.at(linePos) == 'o' && linePos + 2 <= line.size())
-                {
-                    if (line.at(linePos + 1) == 'n')
-                        if (line.at(linePos + 2) == 'e')
-                            foundNumbers.push_back(1);
-                }
-                else if (line.at(linePos) == 't' && linePos + 2 <= line.size())
-                {
-                    if (line.at(linePos + 1) == 'w')
-                    {
-                        if (line.at(linePos + 2) == 'o')
-                        {
-                            foundNumbers.push_back(2);
-                        }
-                    }
-                    else if (line.at(linePos + 1) == 'h' && linePos + 4 <= line.size())
-                    {
-                        if (line.at(linePos + 2) == 'r')
-                        {
-                            if (line.at(linePos + 3) == 'e')
-                            {
-                                if (line.at(linePos + 4) == 'e')
-                                {
-                                    foundNumbers.push_back(3);
-                                }
-                            }
-                        }
-                    }
-                }
-                if (line.at(linePos) == 'f' && linePos + 3 <= line.size())
-                {
-                    if (line.at(linePos + 1) == 'o')
-                    {
-                        if (line.at(linePos + 2) == 'u')
-                        {
-                            if (line.at(linePos + 3) == 'r')
-                            {
-                                foundNumbers.push_back(4);
-                            }
-                        }
-                    }
-                    else if (line.at(linePos + 1) == 'i')
-                    {
-                        if (line.at(linePos + 2) == 'v')
-                        {
-                            if (line.at(linePos + 3) == 'e')
-                            {
-                                foundNumbers.push_back(5);
-                            }
-                        }
-                    }
-                }
-                else if (line.at(linePos) == 's' && linePos + 2 <= line.size())
-                {
-                    if (line.at(linePos + 1) == 'i')
-                    {
-                        if (line.at(linePos + 2) == 'x')
-                        {
-                            foundNumbers.push_back(6);
-                        }
-                    }
-                    else if (line.at(linePos + 1) == 'e' && linePos + 4 <= line.size())
-                    {
-                        if (line.at(linePos + 2) == 'v')
-                        {
-                            if (line.at(linePos + 3) == 'e')
-                            {
-                                if (line.at(linePos + 4) == 'n')
-                                {
-                                    foundNumbers.push_back(7);
-                                }
-                            }
-                        }
-                    }
-                }
-                else if (line.at(linePos) == 'e' && linePos + 4 <= line.size())
-                {
-                    if (line.at(linePos + 1) == 'i')
-                    {
-                        if (line.at(linePos + 2) == 'g')
-                        {
-                            if (line.at(linePos + 3) == 'h')
-                            {
-                                if (line.at(linePos + 4) == 't')
-                                {
-                                    foundNumbers.push_back(8);
-                                }
-                            }
-                        }
-                    }
-                }
-                else if (line.at(linePos) == 'n' && linePos + 3 <= line.size())
-                {
-                    if (line.at(linePos + 1) == 'i')
-                    {
-                        if (line.at(linePos + 2) == 'n')
-                        {
-                            if (line.at(linePos + 3) == 'e')
-                            {
-                                foundNumbers.push_back(9);
-                            }
-                        }
-                    }
-                }
-            }
-            linePos++;
+    int result = 1;
+    vector<int> times = vector<int>();
+    vector<int> records = vector<int>();
+
+    if (getline(inputFile, line)) {
+        while (regex_search(line, match, pattern)) {
+            times.push_back(stoi(match.str()));
+            line = match.suffix().str();
         }
-        if (foundNumbers.size() == 0)
+    }
+    if (getline(inputFile, line)) {
+        while (regex_search(line, match, pattern))
         {
+            records.push_back(stoi(match.str()));
+            line = match.suffix().str();
+        }
+    }
+
+    // input
+    // const T (times[i]) [s], const s (records[i]) [m], const a (1) [m/s^2]
+    // unknown
+    // v [m/s], t [s]
+
+    // equation
+    // s = v * (T - t)
+    // v = t * a
+    // ->
+    // s = (t * a)(T - t)
+    // 0 = - (a * t^2) + (a * T * t^1) - (s * t^0)
+
+    // solver formula
+    // A = -a = -1
+    // B = (a * T) = T depends on input
+    // C = -s
+    // t1,t2 = (-B +- sqrt(B^2 - 4AC)) / 2A
+    // ->
+    // discriminant = T^2 - 4s
+    // t1 = (-T + sqrt(T^2 + 4s)) / -2
+    // t2 = (-T - sqrt(T^2 + 4s)) / -2
+
+    for (int i = 0; i < times.size(); i++) {
+        int discriminant = times[i] * times[i] - 4 * records[i];
+        if (discriminant <= 0) {
             continue;
         }
-        result += (foundNumbers.at(0)) * 10 + (foundNumbers.at(foundNumbers.size() - 1));
+        float discriminantSquared = sqrt((float)discriminant);
+        float t1 = (-times[i] + discriminantSquared) / -2;
+        float t2 = (-times[i] - discriminantSquared) / -2;
+        float higher = max(t1, t2);
+        float lower = min(t1, t2);
+        if (higher == floor(higher)) {
+            higher--;
+            lower++;
+        }
+        result *= (int)floor(higher) - (int)ceil(lower) + 1;
     }
+    
     inputFile.close();
     cout << "The result is: " << result << endl;
     return 0;
 }
-
-// task one 
-// result: 55108
-
-// one      3    
-
-// two      3
-// three    5    X
-
-// four     4    XX
-// five     4    XX
-
-// six      3
-// seven    5    X
-
-// eight    5    X
-// nine     4    XX
-
-// 1. o, t, f, s, e, n
-
-// task two
-// result: 56324
