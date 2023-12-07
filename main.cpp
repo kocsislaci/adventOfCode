61739332c9fb9374795edb0fdbcb60aad7725416
Laszlo Kocsis <kocsislaci95@gmail.com>
2023-12-07 16:27:29 +0100

    day 2 task 1 and added autoformat

--- main.cpp
+++ main.cpp
@@ -1,78 +1,74 @@
-#include <string>
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <regex>
+#include <string>
 
 using namespace std;
 
-const regex pattern("\\d+");
+const regex numberPattern("\\d+");
+const regex wordPattern("\\b\\w+\\b");
 
-int main()
-{
-    ifstream inputFile;
-    inputFile.open("../input6.txt");
+enum Color {
+  RED,
+  GREEN,
+  BLUE,
+};
 
-    smatch match;
-    string line;
+int main() {
+  ifstream inputFile;
+  inputFile.open("../input2.txt");
 
-    int result = 1;
-    vector<unsigned long long> times = vector<unsigned long long>();
-    vector<unsigned long long> records = vector<unsigned long long>();
+  smatch match;
+  string line;
 
-    if (getline(inputFile, line)) {
-        while (regex_search(line, match, pattern)) {
-            times.push_back(stoll(match.str()));
-            line = match.suffix().str();
-        }
+  int result = 0;
+  bool possible = true;
+  vector<int> containing = vector<int>(); // r g b
+  containing.push_back(12);
+  containing.push_back(13);
+  containing.push_back(14);
+  vector<int> numbers = vector<int>();
+  vector<Color> words = vector<Color>();
+
+  while (getline(inputFile, line)) {
+
+    int gameNumber;
+    if (regex_search(line, match, numberPattern)) {
+      gameNumber = stoi(match.str());
     }
-    if (getline(inputFile, line)) {
-        while (regex_search(line, match, pattern))
-        {
-            records.push_back(stoll(match.str()));
-            line = match.suffix().str();
+    while (regex_search(line, match, numberPattern) && possible) {
+      int number = stoi(match.str());
+      line = match.suffix().str();
+      if (regex_search(line, match, wordPattern)) {
+        switch (match.str()[0]) {
+        case 'r':
+          if (number > containing[0]) {
+            possible = false;
+          }
+          break;
+        case 'g':
+          if (number > containing[1]) {
+            possible = false;
+          }
+          break;
+        case 'b':
+          if (number > containing[2]) {
+            possible = false;
+          }
+          break;
+        default:
+          break;
         }
+      }
     }
-
-    // input
-    // const T (times[i]) [s], const s (records[i]) [m], const a (1) [m/s^2]
-    // unknown
-    // v [m/s], t [s]
-
-    // equation
-    // s = v * (T - t)
-    // v = t * a
-    // ->
-    // s = (t * a)(T - t)
-    // 0 = - (a * t^2) + (a * T * t^1) - (s * t^0)
-
-    // solver formula
-    // A = -a = -1
-    // B = (a * T) = T depends on input
-    // C = -s
-    // t1,t2 = (-B +- sqrt(B^2 - 4AC)) / 2A
-    // ->
-    // discriminant = T^2 - 4s
-    // t1 = (-T + sqrt(T^2 + 4s)) / -2
-    // t2 = (-T - sqrt(T^2 + 4s)) / -2
-
-    for (int i = 0; i < times.size(); i++) {
-        unsigned long long discriminant = times[i] * times[i] - 4 * records[i];
-        if (discriminant <= 0) {
-            continue;
-        }
-        long double discriminantSquared = sqrt((long double)discriminant);
-        long double t1 = ((long double)times[i] - discriminantSquared) / (long double)2;
-        long double t2 = ((long double)times[i] + discriminantSquared) / (long double)2;
-        long double higher = max(t1, t2);
-        long double lower = min(t1, t2);
-        if (higher == floor(higher)) {
-            higher--;
-            lower++;
-        }
-        result *= (unsigned long long)floor(higher) - (unsigned long long)ceil(lower) + 1;
+    if (possible) {
+      result += gameNumber;
+    } else {
+      possible = true;
     }
-    
-    inputFile.close();
-    cout << "The result is: " << result << endl;
-    return 0;
+  }
+
+  inputFile.close();
+  cout << "The result is: " << result << endl;
+  return 0;
 }
