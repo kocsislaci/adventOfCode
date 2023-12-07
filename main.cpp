8bee22d003ba14bd485462f482c41262cda28152
Laszlo Kocsis <kocsislaci95@gmail.com>
2023-12-07 23:33:27 +0100

    day 3 part 1 unoptimized

--- main.cpp
+++ main.cpp
@@ -8,61 +8,89 @@ using namespace std;
 const regex numberPattern("\\d+");
 const regex wordPattern("\\b\\w+\\b");
 
-enum Color {
-  RED,
-  GREEN,
-  BLUE,
-};
+// we need 3 lines at once
 
 int main() {
   ifstream inputFile;
-  inputFile.open("./input2.txt");
+  inputFile.open("./input3.txt");
 
   smatch match;
   string line;
+  vector<string> lines = vector<string>(3); // keep size 3
 
   int result = 0;
-  vector<int> colorMax = vector<int>(); // r g b
-  colorMax.push_back(0);
-  colorMax.push_back(0);
-  colorMax.push_back(0);
+  bool forTheLastLine = true;
 
-  while (getline(inputFile, line)) {
-    colorMax[0] = 0;
-    colorMax[1] = 0;
-    colorMax[2] = 0;
-    if (regex_search(line, match, numberPattern)) {
-      // we get the first number to get the needed numbers later
+  while (getline(inputFile, line) || forTheLastLine) {
+    lines[2] = lines[1]; // new line then drop the last one
+    lines[1] = lines[0];
+    lines[0] = line;
+
+    if (lines[0].empty()) {
+      forTheLastLine = false;
     }
-    while (regex_search(line, match, numberPattern)) {
-      int number = stoi(match.str());
-      line = match.suffix().str();
-      if (regex_search(line, match, wordPattern)) {
-        switch (match.str()[0]) {
-        case 'r':
-          if (number > colorMax[0]) {
-            colorMax[0] = number;
+
+    int X = 0;
+    string acc = "";
+    bool canCalc = false, isLast = false;
+
+    while (X < lines[1].size()) { // check line along X
+
+      if (isdigit(lines[1][X])) {
+
+        acc += lines[1][X];
+
+        if ((X + 1) == lines[1].size()) { // last in the row
+          isLast = true;
+        }
+        if (!isdigit(lines[1][X + 1]) &&
+            (X + 1) < lines[1].size()) { // right neighbour
+          isLast = true;
+        }
+
+        for (int dx = -1; dx < 2; dx++) {
+          for (int dy = -1; dy < 2; dy++) {
+
+            if (canCalc) { // already found special char for this accumulated
+                           // integer
+              continue;
+            }
+
+            if (lines[dy + 1].empty()) { // empty line
+              continue;
+            }
+
+            if (dy == 0 && dx == 0 || (-1 == X + dx) ||
+                (lines[1].size() == X + dx)) { // middle and out of bounds
+              continue;
+            }
+
+            if (!isdigit(lines[dy + 1][X + dx]) &&
+                lines[dy + 1][X + dx] != '.') { // finish
+              canCalc = true;
+              dx = 2;
+              dy = 2;
+            }
           }
-          break;
-        case 'g':
-          if (number > colorMax[1]) {
-            colorMax[1] = number;
+        }
+        if (isLast) {
+          if (acc.size() > 0 && canCalc) {
+            result += stoi(acc);
           }
-          break;
-        case 'b':
-          if (number > colorMax[2]) {
-            colorMax[2] = number;
-          }
-          break;
-        default:
-          break;
+          canCalc = false;
+          isLast = false;
+          acc = "";
         }
       }
+      X++;
     }
-    result += colorMax[0] * colorMax[1] * colorMax[2];
   }
 
   inputFile.close();
   cout << "The result is: " << result << endl;
   return 0;
 }
+
+// test data:
+// 467+35+633+617+592+755+664+598
+// result shoudl be: 4361
