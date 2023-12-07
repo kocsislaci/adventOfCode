af8d32ab1a2d0e67bb2fab4e951c7d7667371218
Laszlo Kocsis <kocsislaci95@gmail.com>
2023-12-07 16:40:47 +0100

    day1.h formatted

--- day1.h
+++ day1.h
@@ -1,143 +1,103 @@
-#include <string>
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-int day1()
-{
-    ifstream inputFile;
-    inputFile.open("./input.txt");
+int day1() {
+  ifstream inputFile;
+  inputFile.open("./input.txt");
 
-    int result = 0;
-    string line;
-    while (getline(inputFile, line))
-    {
-        int linePos = 0;
-        vector<int> foundNumbers = vector<int>();
-        while (linePos != line.size())
-        {
-            if (isdigit(line.at(linePos)))
-                foundNumbers.push_back(line.at(linePos) - 48); // offset to 0-9
-            else
-            {
+  int result = 0;
+  string line;
+  while (getline(inputFile, line)) {
+    int linePos = 0;
+    vector<int> foundNumbers = vector<int>();
+    while (linePos != line.size()) {
+      if (isdigit(line.at(linePos)))
+        foundNumbers.push_back(line.at(linePos) - 48); // offset to 0-9
+      else {
 
-                if (line.at(linePos) == 'o' && linePos + 2 <= line.size())
-                {
-                    if (line.at(linePos + 1) == 'n')
-                        if (line.at(linePos + 2) == 'e')
-                            foundNumbers.push_back(1);
-                }
-                else if (line.at(linePos) == 't' && linePos + 2 <= line.size())
-                {
-                    if (line.at(linePos + 1) == 'w')
-                    {
-                        if (line.at(linePos + 2) == 'o')
-                        {
-                            foundNumbers.push_back(2);
-                        }
-                    }
-                    else if (line.at(linePos + 1) == 'h' && linePos + 4 <= line.size())
-                    {
-                        if (line.at(linePos + 2) == 'r')
-                        {
-                            if (line.at(linePos + 3) == 'e')
-                            {
-                                if (line.at(linePos + 4) == 'e')
-                                {
-                                    foundNumbers.push_back(3);
-                                }
-                            }
-                        }
-                    }
-                }
-                if (line.at(linePos) == 'f' && linePos + 3 <= line.size())
-                {
-                    if (line.at(linePos + 1) == 'o')
-                    {
-                        if (line.at(linePos + 2) == 'u')
-                        {
-                            if (line.at(linePos + 3) == 'r')
-                            {
-                                foundNumbers.push_back(4);
-                            }
-                        }
-                    }
-                    else if (line.at(linePos + 1) == 'i')
-                    {
-                        if (line.at(linePos + 2) == 'v')
-                        {
-                            if (line.at(linePos + 3) == 'e')
-                            {
-                                foundNumbers.push_back(5);
-                            }
-                        }
-                    }
-                }
-                else if (line.at(linePos) == 's' && linePos + 2 <= line.size())
-                {
-                    if (line.at(linePos + 1) == 'i')
-                    {
-                        if (line.at(linePos + 2) == 'x')
-                        {
-                            foundNumbers.push_back(6);
-                        }
-                    }
-                    else if (line.at(linePos + 1) == 'e' && linePos + 4 <= line.size())
-                    {
-                        if (line.at(linePos + 2) == 'v')
-                        {
-                            if (line.at(linePos + 3) == 'e')
-                            {
-                                if (line.at(linePos + 4) == 'n')
-                                {
-                                    foundNumbers.push_back(7);
-                                }
-                            }
-                        }
-                    }
-                }
-                else if (line.at(linePos) == 'e' && linePos + 4 <= line.size())
-                {
-                    if (line.at(linePos + 1) == 'i')
-                    {
-                        if (line.at(linePos + 2) == 'g')
-                        {
-                            if (line.at(linePos + 3) == 'h')
-                            {
-                                if (line.at(linePos + 4) == 't')
-                                {
-                                    foundNumbers.push_back(8);
-                                }
-                            }
-                        }
-                    }
-                }
-                else if (line.at(linePos) == 'n' && linePos + 3 <= line.size())
-                {
-                    if (line.at(linePos + 1) == 'i')
-                    {
-                        if (line.at(linePos + 2) == 'n')
-                        {
-                            if (line.at(linePos + 3) == 'e')
-                            {
-                                foundNumbers.push_back(9);
-                            }
-                        }
-                    }
-                }
+        if (line.at(linePos) == 'o' && linePos + 2 <= line.size()) {
+          if (line.at(linePos + 1) == 'n')
+            if (line.at(linePos + 2) == 'e')
+              foundNumbers.push_back(1);
+        } else if (line.at(linePos) == 't' && linePos + 2 <= line.size()) {
+          if (line.at(linePos + 1) == 'w') {
+            if (line.at(linePos + 2) == 'o') {
+              foundNumbers.push_back(2);
             }
-            linePos++;
+          } else if (line.at(linePos + 1) == 'h' &&
+                     linePos + 4 <= line.size()) {
+            if (line.at(linePos + 2) == 'r') {
+              if (line.at(linePos + 3) == 'e') {
+                if (line.at(linePos + 4) == 'e') {
+                  foundNumbers.push_back(3);
+                }
+              }
+            }
+          }
         }
-        if (foundNumbers.size() == 0)
-        {
-            continue;
+        if (line.at(linePos) == 'f' && linePos + 3 <= line.size()) {
+          if (line.at(linePos + 1) == 'o') {
+            if (line.at(linePos + 2) == 'u') {
+              if (line.at(linePos + 3) == 'r') {
+                foundNumbers.push_back(4);
+              }
+            }
+          } else if (line.at(linePos + 1) == 'i') {
+            if (line.at(linePos + 2) == 'v') {
+              if (line.at(linePos + 3) == 'e') {
+                foundNumbers.push_back(5);
+              }
+            }
+          }
+        } else if (line.at(linePos) == 's' && linePos + 2 <= line.size()) {
+          if (line.at(linePos + 1) == 'i') {
+            if (line.at(linePos + 2) == 'x') {
+              foundNumbers.push_back(6);
+            }
+          } else if (line.at(linePos + 1) == 'e' &&
+                     linePos + 4 <= line.size()) {
+            if (line.at(linePos + 2) == 'v') {
+              if (line.at(linePos + 3) == 'e') {
+                if (line.at(linePos + 4) == 'n') {
+                  foundNumbers.push_back(7);
+                }
+              }
+            }
+          }
+        } else if (line.at(linePos) == 'e' && linePos + 4 <= line.size()) {
+          if (line.at(linePos + 1) == 'i') {
+            if (line.at(linePos + 2) == 'g') {
+              if (line.at(linePos + 3) == 'h') {
+                if (line.at(linePos + 4) == 't') {
+                  foundNumbers.push_back(8);
+                }
+              }
+            }
+          }
+        } else if (line.at(linePos) == 'n' && linePos + 3 <= line.size()) {
+          if (line.at(linePos + 1) == 'i') {
+            if (line.at(linePos + 2) == 'n') {
+              if (line.at(linePos + 3) == 'e') {
+                foundNumbers.push_back(9);
+              }
+            }
+          }
         }
-        result += (foundNumbers.at(0)) * 10 + (foundNumbers.at(foundNumbers.size() - 1));
+      }
+      linePos++;
     }
-    inputFile.close();
-    cout << "The result is: " << result << endl;
-    return 0;
+    if (foundNumbers.size() == 0) {
+      continue;
+    }
+    result +=
+        (foundNumbers.at(0)) * 10 + (foundNumbers.at(foundNumbers.size() - 1));
+  }
+  inputFile.close();
+  cout << "The result is: " << result << endl;
+  return 0;
 }
 
 // task one
